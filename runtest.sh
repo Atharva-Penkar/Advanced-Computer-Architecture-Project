f87d7dd297e55a8e2d0790bc238272945d355550
Aditya Singh <adityasingh@researchcommons.ai>
2025-11-10 03:41:45 +0530

    Added prefetcher to shell script

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,7 @@
 cd ~/ACA/simplescalar/simplesim-3.0
 
-./sim-cache -config ~/ACA/cacheconfig.txt ~/ACA/simplescalar/simplesim-3.0/tests-pisa/bin.little/test-math
+#./sim-cache -config ~/ACA/cacheconfig.txt ~/ACA/simplescalar/simplesim-3.0/tests-pisa/bin.little/test-math
 #./sim-cache -config ~/ACA/cacheconfig.txt ../spec95-little/vortex.ss ~/ACA/inputs/vortex.in > ~/ACA/results/vortex_cache_output.txt
+
+./sim-cache -prefetcher next_line -config ~/ACA/cacheconfig.txt ~/ACA/simplescalar/simplesim-3.0/tests-pisa/bin.little/test-math > result_nextline.txt
+./sim-cache -prefetcher stride -config ~/ACA/cacheconfig.txt ~/ACA/simplescalar/simplesim-3.0/tests-pisa/bin.little/test-math > result_stride.txt
