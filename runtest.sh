70f0790ced8015aa65c2dd14a624b7cac4f68ebe
Kumar Snehal <kssnehal605@gmail.com>
2025-11-09 12:17:38 +0000

    LFU working

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,4 @@
 cd ~/ACA/simplescalar/simplesim-3.0
 
-./sim-cache -config ~/ACA/cacheconfig.txt ../spec95-little/vortex.ss ~/ACA/inputs/vortex.in > ~/ACA/results/vortex_cache_output.txt
+./sim-cache -config ~/ACA/cacheconfig.txt ~/ACA/simplescalar/simplesim-3.0/tests-pisa/bin.little/test-math
+#./sim-cache -config ~/ACA/cacheconfig.txt ../spec95-little/vortex.ss ~/ACA/inputs/vortex.in > ~/ACA/results/vortex_cache_output.txt
