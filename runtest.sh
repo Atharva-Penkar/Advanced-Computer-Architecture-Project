8b017427a33725a87956abd6cb02b59b42b96d1f
Aditya Singh <adityasingh@researchcommons.ai>
2025-11-10 03:46:51 +0530

    Corrected path in shell

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,4 @@
-cd ~/ACA/simplescalar/simplesim-3.0
+cd ~/Advanced-Computer-Architecture-Project/simplescalar/simplesim-3.0
 
 #./sim-cache -config ~/ACA/cacheconfig.txt ~/ACA/simplescalar/simplesim-3.0/tests-pisa/bin.little/test-math
 #./sim-cache -config ~/ACA/cacheconfig.txt ../spec95-little/vortex.ss ~/ACA/inputs/vortex.in > ~/ACA/results/vortex_cache_output.txt
